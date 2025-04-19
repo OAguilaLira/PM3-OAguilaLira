95d2c7c4819adf46b7b7eb996daf8ad96be3d54f
Omar <omarguilalira2@gmail.com>
2025-04-18 23:16:55 -0600

    Actualizaciones

--- back/dist/index.js
+++ back/dist/index.js
@@ -1,5 +1,15 @@
 "use strict";
-const saludoDesdeTypeScript = (name) => {
-    console.log(`Hola ${name} desde TypeScript`);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-saludoDesdeTypeScript('Omar');
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const server_1 = __importDefault(require("./server"));
+const envs_1 = require("./config/envs");
+const data_source_1 = require("./config/data-source");
+data_source_1.AppDataSource.initialize().then(res => {
+    console.log('Conexión exitosa con la base de datos');
+    server_1.default.listen(envs_1.PORT, () => {
+        console.log(`Servidor escuchando en el puerto ${envs_1.PORT}`);
+    });
+});
--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -1,21 +1,21 @@
-import { DataSource } from "typeorm"
-import { UserEntity } from "../entities/User"
-import { CredentialEntity } from "../entities/Credential"
-import { AppointmentEntity } from "../entities/Appointment"
-
+import { DataSource } from "typeorm";
+import { UserEntity } from "../entities/User";
+import { CredentialEntity } from "../entities/Credential";
+import { AppointmentEntity } from "../entities/Appointment";
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME } from "./envs";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "1237515",
-    database: "turnos",
-    entities: [UserEntity, CredentialEntity, AppointmentEntity],
-    synchronize: true,
-    logging: ["error"],
-    dropSchema: false,
-})
+  type: "postgres",
+  host: DB_HOST,
+  port: Number(DB_PORT),
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  entities: [UserEntity, CredentialEntity, AppointmentEntity],
+  synchronize: true,
+  logging: ["error"],
+  dropSchema: false,
+});
 
 export const UserModel = AppDataSource.getRepository(UserEntity);
 export const CredentialModel = AppDataSource.getRepository(CredentialEntity);
