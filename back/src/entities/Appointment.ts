ec865259285f58a509b5b1140b2bc16874f3f930
Omar <omaraguilalira2@gmail.com>
2025-04-25 23:11:15 -0600

    Change ID type to uuid, update service types handling, and add service comments

--- back/src/entities/Appointment.ts
+++ back/src/entities/Appointment.ts
@@ -2,18 +2,26 @@ import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import StatusAppointment from "../enums/StatusAppointment";
 import { UserEntity } from "./User";
 
-@Entity({name: "appointments"})
+@Entity({ name: "appointments" })
 export class AppointmentEntity {
-    @PrimaryGeneratedColumn()
-    id: number
-    @Column({type: "timestamptz"})
-    date: Date
-    @Column({type: "timestamptz"})
-    time: Date
-    @Column()
-    status: StatusAppointment
-    @ManyToOne(() => UserEntity, (user) => user.appointments)
-    user: UserEntity
-    
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+  @Column({ type: "timestamptz" })
+  date: Date;
+  // @Column({type: "timestamptz"})
+  // time: Date
+  @Column()
+  serviceType: string;
+  @Column()
+  notes: string;
+  @Column()
+  duration: number;
+  @Column({
+    type: "enum",
+    enum: StatusAppointment,
+    default: StatusAppointment.ACTIVE,
+  })
+  status: StatusAppointment;
+  @ManyToOne(() => UserEntity, (user) => user.appointments)
+  user: UserEntity;
 }
-
